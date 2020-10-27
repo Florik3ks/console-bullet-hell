bb9e992664760dc1ca50d3dbf64b8c6ab8279546
Florik3ks <floritrs@gmail.com>
2020-10-27 15:35:00 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_jump_n_run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while(true){
+                ;
+            }
+        }
+    }
+}
